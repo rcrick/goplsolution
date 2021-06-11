0371df7ed046f965ed220201ab0c8ff3c7cc9379
zixuan.xu <zixuan.xu@shopee.com>
2021-06-12 01:16:51 +0800

    commit

---
+++ solution/ex1.7/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			continue
+		}
+		defer resp.Body.Close()
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		}
+	}
+}
